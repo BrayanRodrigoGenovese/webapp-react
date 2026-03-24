f4023f8345989ec9bd4e1af8ae379bda38c5935b
Brayan <bittolo.8bit@gmail.com>
2026-03-24 16:57:47 +0100

    adding a footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,16 @@
 function Footer() {
-    return <></>;
+    const currentYear = new Date().getFullYear();
+
+    return (
+        <footer className="bg-dark text-white text-center py-4">
+            <div className="container">
+                <p className="mb-0">
+                    &copy; {currentYear} Brayan Rodrigo Genovese Movies. Tutti i
+                    diritti riservati.
+                </p>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
