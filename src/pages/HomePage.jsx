e98fc9d955d642e92b437ca37f0342a5631e73cf
Brayan <bittolo.8bit@gmail.com>
2026-03-24 17:00:14 +0100

    adding homepage structure and logic

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,56 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { useLoader } from "../contexts/LoaderContext";
 
 function HomePage() {
     const [movies, setMovies] = useState([]);
+    const { startLoading, endLoading } = useLoader();
+
+    useEffect(() => {
+        startLoading();
+
+        axios
+            .get("http://localhost:3000/movies")
+            .then((response) => {
+                setMovies(response.data);
+            })
+            .catch((error) => {
+                console.error("Errore durante il recupero dei film:", error);
+            })
+            .finally(() => endLoading());
+    }, []);
 
     return (
         <div>
-            <h1>Lista dei Film</h1>
+            <h1 className="mb-4">Lista dei Film</h1>
+            <div className="row g-4">
+                {movies.map((movie) => (
+                    <div className="col-12 col-md-6 col-lg-4" key={movie.id}>
+                        <div className="card h-100 shadow-sm">
+                            <img
+                                src={`http://localhost:3000/movies_cover/${movie.image}`}
+                                className="card-img-top"
+                                alt={`Locandina del film ${movie.title}`}
+                                style={{ height: "400px", objectFit: "cover" }}
+                            />
+                            <div className="card-body d-flex flex-column">
+                                <h5 className="card-title">{movie.title}</h5>
+                                <p className="card-text text-muted">
+                                    {movie.director}
+                                </p>
+
+                                <Link
+                                    to={`/movies/${movie.id}`}
+                                    className="btn btn-primary mt-auto"
+                                >
+                                    Vedi Dettagli
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
