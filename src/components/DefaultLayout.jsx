0c0e78b3cc3426365b33d7e93f854b6e62ad0e1f
Brayan <bittolo.8bit@gmail.com>
2026-03-24 16:57:27 +0100

    improving default layout

--- src/components/DefaultLayout.jsx
+++ src/components/DefaultLayout.jsx
@@ -1,16 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Loader from "./Loader";
 
 function DefaultLayout() {
     return (
-        <>
+        <div className="d-flex flex-column min-vh-100">
+            <Loader />
+
             <Navbar />
-            <main className="container my-5">
+            <main className="container my-5 flex-grow-1">
                 <Outlet />
             </main>
             <Footer />
-        </>
+        </div>
     );
 }
 
