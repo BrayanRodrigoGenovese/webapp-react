80f228603b53453751b2843bedff9a382752a525
Brayan <bittolo.8bit@gmail.com>
2026-03-24 16:57:38 +0100

    adding a navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
-    return <></>;
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+            <div className="container">
+                <Link className="navbar-brand fw-bold fs-3" to="/">
+                    MOVIES BLOG
+                </Link>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
